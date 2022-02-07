5e8a62649cf332302b2c45f552c270da0482b58c
Pooja.Singh <Pooja.Singh@JKTNOLAP627.AD.JKTECH.COM>
2022-02-07 12:12:27 +0530

    First commit of day.

--- TestProject/src/test/java/Demo.java
+++ TestProject/src/test/java/Demo.java
@@ -27,7 +27,7 @@ public class Demo {
 
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,1000)");
-		//Test..
+		//Test..123
 		List<WebElement> dresses= driver.findElements(By.xpath("//img[@title='Printed Summer Dress']"));
 		System.out.println(dresses.size());
 	/*	for(int i=0;i<=dresses.size()-1;i++) {
